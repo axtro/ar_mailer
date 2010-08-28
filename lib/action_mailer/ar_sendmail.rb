061ba8c7efcb7c14bb1c433f6605425aca806c7e
Henning Kiel <henning.kiel@gmail.com>
2010-08-28 16:09:29 +0200

    Version 2.2.0

--- lib/action_mailer/ar_sendmail.rb
+++ lib/action_mailer/ar_sendmail.rb
@@ -42,7 +42,7 @@ class ActionMailer::ARSendmail
   ##
   # The version of ActionMailer::ARSendmail you are running.
 
-  VERSION = '2.1.6'
+  VERSION = '2.2.0'
 
   ##
   # Maximum number of times authentication will be consecutively retried
